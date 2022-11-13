1ea909c6313ee983effa10de97ab70335529818d
Jinseon Lee <memilmuk82@gmail.com>
2022-11-13 21:32:26 +0900

    Map v1.9

--- 2023_map_sep.py
+++ 2023_map_sep.py
@@ -3,7 +3,6 @@ import folium
 
 # 파일명.csv 파일 전처리 후 사용 -> 학교명, 지원학과 항목만 
 
-
 map1 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
 SchoolData1 = pre_Data('ahyeon.csv', '아현', '정시') # 학교 데이터 입력 / pre_Data('파일명.csv', '학교명', '정시 또는 추가')
 map1 = Map_Data(Merge_Data(SchoolData1), map1, "#00B8A9", "#00B8A9", 0.5, 1) # Map_Data(Merge_Data(SchoolData), map, '외곽선 색', '채우기 색', 투명도(0.0 ~ 1.0), 팝업표시여부(1, 0)) 
