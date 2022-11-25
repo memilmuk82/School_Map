91af90a69253c039232db109860360f9a772c68f
Jinseon Lee <memilmuk82@gmail.com>
2022-11-25 14:00:49 +0900

    School_Map_V3.0

--- 2023_map.py
+++ 2023_map.py
@@ -4,20 +4,24 @@ import folium
 # 파일명.csv 파일 전처리 후 사용 -> 학교명, 지원학과 항목만 
 map = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
 
-filename = './data/ahyeon.csv' 
-SchoolData = pre_Data(filename) # 학교별 합격자 수 정리 
+schoolname = './data/ahyeon.csv' 
+SchoolData = pre_Data(schoolname) # 학교별 합격자 수 정리 
 map = Map_Data(Merge_Data(SchoolData), map, "#00B8A9", "#00B8A9", 0.5, 0) # Map_Data(Merge_Data(SchoolData), map, '외곽선 색', '채우기 색', 투명도(0.0 ~ 1.0), 팝업표시(표시:1, 미표시:0)) 
 
 schoolname = './data/enpyeong.csv' 
-SchoolData = pre_Data(filename)
+SchoolData = pre_Data(schoolname)
 map = Map_Data(Merge_Data(SchoolData), map, "#541212", "#541212", 0.5, 0)
 
 schoolname = './data/geumcheon.csv' 
-SchoolData = pre_Data(filename)
-map = Map_Data(Merge_Data(SchoolData), map, "#7900FF", "#7900FF", 0.3, 1)
+SchoolData = pre_Data(schoolname)
+map = Map_Data(Merge_Data(SchoolData), map, "#7900FF", "#7900FF", 0.5, 0)
+
+schoolname = './data/seocho.csv' 
+SchoolData = pre_Data(schoolname)
+map = Map_Data(Merge_Data(SchoolData), map, "#764AF1", "#764AF1", 0.5, 0)
 
 schoolname = './data/jongno.csv'
-SchoolData = pre_Data(filename)
+SchoolData = pre_Data(schoolname)
 map = Map_Data(Merge_Data(SchoolData), map, "#FF1E1E", "#FF1E1E", 0.3, 1)
 
 map.save('./html/2023map.html') # html 파일로 저장 / map.save('파일명.html')
--- 2023_map_sep.py
+++ 2023_map_sep.py
@@ -3,22 +3,32 @@ import folium
 
 # 파일명.csv 파일 전처리 후 사용 -> 학교명, 지원학과 항목만 
 
+schoolname1 = './data/ahyeon.csv'
 map1 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
-SchoolData1 = pre_Data('ahyeon.csv', '아현', '정시') # 학교 데이터 입력 / pre_Data('파일명.csv', '학교명', '정시 또는 추가')
+SchoolData1 = pre_Data(schoolname1) # 학교 데이터 입력 / pre_Data('파일명.csv', '학교명', '정시 또는 추가')
 map1 = Map_Data(Merge_Data(SchoolData1), map1, "#00B8A9", "#00B8A9", 0.5, 1) # Map_Data(Merge_Data(SchoolData), map, '외곽선 색', '채우기 색', 투명도(0.0 ~ 1.0), 팝업표시여부(1, 0)) 
-map1.save('2023_ahyeon.html') # html 파일로 저장 / map.save('파일명.html')
+map1.save('./html/2023_ahyeon.html') # html 파일로 저장 / map.save('파일명.html')
 
+schoolname2 = './data/enpyeong.csv'
 map2 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
-SchoolData2 = pre_Data('enpyeong.csv', '은평', '정시')
+SchoolData2 = pre_Data(schoolname2)
 map2 = Map_Data(Merge_Data(SchoolData2), map2, "#541212", "#541212", 0.5, 1)
-map2.save('2023_enpyeong.html') # html 파일로 저장 / map.save('파일명.html')
+map2.save('./html/2023_enpyeong.html') # html 파일로 저장 / map.save('파일명.html')
 
+schoolname3 = './data/geumcheon.csv'
 map3 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
-SchoolData3 = pre_Data('geumcheon.csv', '금천', '정시')
+SchoolData3 = pre_Data(schoolname3)
 map3 = Map_Data(Merge_Data(SchoolData3), map3, "#7900FF", "#7900FF", 0.3, 1)
-map3.save('2023_geumcheon.html') # html 파일로 저장 / map.save('파일명.html')
+map3.save('./html/2023_geumcheon.html') # html 파일로 저장 / map.save('파일명.html')
 
+schoolname4 = './data/seocho.csv'
 map4 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
-SchoolData4 = pre_Data('jongno.csv', '종로', '정시')
-map4 = Map_Data(Merge_Data(SchoolData4), map4, "#FF1E1E", "#FF1E1E", 0.3, 1)
-map4.save('2023_jongno.html') # html 파일로 저장 / map.save('파일명.html')
+SchoolData4 = pre_Data(schoolname4)
+map4 = Map_Data(Merge_Data(SchoolData4), map4, "#764AF1", "#764AF1", 0.3, 1)
+map4.save('./html/2023_seocho.html') # html 파일로 저장 / map.save('파일명.html')
+
+schoolname5 = './data/jongno.csv'
+map5 = folium.Map(location=[37.55,126.98], zoom_start=12) # 기본맵 설정
+SchoolData5 = pre_Data(schoolname5)
+map5 = Map_Data(Merge_Data(SchoolData5), map5, "#FF1E1E", "#FF1E1E", 0.3, 1)
+map5.save('./html/2023_jongno.html') # html 파일로 저장 / map.save('파일명.html')
